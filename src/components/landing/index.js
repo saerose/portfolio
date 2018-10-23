125c3090835b1516f52c3ab97f98c787d5f39712
saerose <sbarcelocompte@gmail.com>
2018-10-23 19:42:15 +0200

    chore: modified styles

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -11,9 +11,11 @@ class Landing extends Component {
     return (
       <div className='landing__wrapper'>
         <div className='landing__intro__wrapper'>
-          <SpeechBubble size={220} mood='lovestruck' color='#83D1FB' />
+          <SpeechBubble size={220} mood='lovestruck' color='#7FB3D5' />
           <div className='landing__intro__name'>Hello, I'm Sae!</div>
+          <div className='landing__intro__description'>Front End Dev | Part-Time Doodler | Historic Costumaholic | Full Time Nerd</div>
           <Social />
+          go down
           </div>
       </div>
     );
